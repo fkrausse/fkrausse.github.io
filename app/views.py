c1aca90a9049db503b1147892716721b6968c794
Fabian Adolfo <fabi_adolfo@hotmail.com>
2020-09-29 01:17:23 -0400

    Finalizado la primera parte de la base, la pagina sobre Fabian y también iniciado el primer articulo que trata sobre Power BI

--- app/views.py
+++ app/views.py
@@ -4,6 +4,13 @@ from main import app
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response= make_response(render_template('index.html'))
+    response= make_response(render_template('sobre.html'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/article')
+def article():
+    user_ip = request.remote_addr
+    response= make_response(render_template('article_1.html'))
     response.set_cookie('user_ip', user_ip)
     return response
