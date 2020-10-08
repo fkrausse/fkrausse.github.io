1d576aa6ac7fd038814e3748b4a1da2af6614198
Fabian Adolfo <fabi_adolfo@hotmail.com>
2020-10-08 09:21:48 -0300

    Agregado articulo 2

--- app/views.py
+++ app/views.py
@@ -8,9 +8,23 @@ def index():
     response.set_cookie('user_ip', user_ip)
     return response
 
+@app.route('/historia')
+def historia():
+    user_ip = request.remote_addr
+    response= make_response(render_template('historia.html'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
 @app.route('/article')
 def article():
     user_ip = request.remote_addr
     response= make_response(render_template('article_1.html'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/article2')
+def article2():
+    user_ip = request.remote_addr
+    response= make_response(render_template('article_2.html'))
     response.set_cookie('user_ip', user_ip)
     return response
